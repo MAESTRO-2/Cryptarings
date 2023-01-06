b2d84388110392902b6cb4b3a70709cb06b75ef4
saad <saddaddina@gmail.com>
2023-01-06 14:01:00 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router }from "react-router-dom";
+import { HashRouter }from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from "./componant/app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <Provider store={store} >
         <App />
       </Provider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 );
 
